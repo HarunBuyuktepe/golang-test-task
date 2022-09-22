a6b1f9686569b53a43d26f386807b71c0614cf4a
Harun Büyüktepe <hrnbyktp18@gmail.com>
2022-09-22 17:39:34 +0300

    first commit before docker compose

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+type Message struct {
+	Sender   string `json:"sender" binding:"required"`
+	Receiver string `json:"receiver" binding:"required"`
+	Message  string `json:"message" binding:"required"`
+}
 
 func main() {
 	r := gin.Default()
@@ -10,6 +18,21 @@ func main() {
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, "worked")
 	})
+	r.POST("/message", postMessageHandler)
 
-	r.Run()
+	r.Run("localhost:8080")
+}
+
+func postMessageHandler(c *gin.Context) {
+	var message Message
+
+	// Call BindJSON to bind the received JSON to
+	if err := c.ShouldBindJSON(&message); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "bad request"})
+		return
+	}
+
+	fmt.Println(message)
+	// push to rabbitmq queue
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "got message success"})
 }
